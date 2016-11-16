476185697f1adbf9d02f3c98adbfb51672c43668
phuquoc <votruongtoan123@gmail.com>
2016-11-16 23:47:47 +0700

    Comment
    Commented

--- nhap/config/routes.rb
+++ nhap/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+#  get 'comment/create'
+
   #get '/blogs', to: 'blogs#index'
   resources :blogs, only: [:index, :new, :create,
-  :show, :edit, :update,:destroy]
+  :show, :edit, :update,:destroy] do
+      resources :comments, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello',to: 'hello#index'
   post '/hello',to: 'hello#say_hello'
--- nhap/db/seeds.rb
+++ nhap/db/seeds.rb
@@ -1,3 +1,8 @@
 Blog.delete_all
-Blog.create!(title: 'test', content: 'for test')
+Comment.delete_all
+article = Blog.create!(title: 'test', content: 'for test')
 Blog.create!(title: 'test2', content: 'for test2')
+Comment.create!(name: 'quoc', content: 'phu quoc commented',
+blog_id: article.id)
+Comment.create!(name: 'Nho', content: 'Phan Anh commented',
+blog_id: article.id)
